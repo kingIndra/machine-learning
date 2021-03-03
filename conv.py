4ae58651dd621170c1069def891839a60ec28b6e
Ashutosh Roy <ashutoshr@Cenduit.com>
2021-03-03 23:43:24 +0530

    v1.7

--- conv.py
+++ conv.py
@@ -1,11 +1,4 @@
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import LabelEncoder
-import os
-
 #PATH = 'datasets/housing.csv'
 #data = pd.read_csv(PATH)
 
